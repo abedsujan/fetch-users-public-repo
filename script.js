24e80d70096736cd7aeef6f35e4dcca3e9df513c
Abed Sujan <abedsujan@gmail.com>
2018-06-06 00:01:32 +0200

    feat temporarily store user and repo json response in variable

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
-console.log('all set!');
+const searchBtn = document.getElementById("search-btn");
+
+
+searchBtn.addEventListener('click', function () {
+    username = document.getElementById("searchText").value;
+    console.log('search text', username);
+
+    fetchUserInfo(username);
+    fetchUserRepos(username);
+});
+
+
+function fetchUserInfo(username) {
+
+    console.log('useRepos', userInfoJson);
+
+    // fetch('https://api.github.com/users/' + username).then(res => res.json())
+    //     .catch(error => console.error('Error:', error))
+    //     .then((response) => {
+    //        console.log('userinfo', response);
+    //     });
+}
+
+function fetchUserRepos() {
+    console.log('useRepos', reposJson);
+
+    // fetch('https://api.github.com/users/' + username + '/repos?sort=updated').then(res => res.json())
+    //     .catch(error => console.error('Error:', error))
+    //     .then((response) => {
+    //         console.log('useRepos', response);
+    //     });
+}
