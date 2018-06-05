cf3c28f10ab905a72625b6392b225279bc10d243
Abed Sujan <abedsujan@gmail.com>
2018-06-06 00:07:54 +0200

    render user basic info using template literal

--- script.js
+++ script.js
@@ -9,10 +9,14 @@ searchBtn.addEventListener('click', function () {
     fetchUserRepos(username);
 });
 
-
+/*
+    User basic info
+*/
 function fetchUserInfo(username) {
 
     console.log('useRepos', userInfoJson);
+
+    renderUserInfoTemplate(userInfoJson);
 
     // fetch('https://api.github.com/users/' + username).then(res => res.json())
     //     .catch(error => console.error('Error:', error))
@@ -21,6 +25,42 @@ function fetchUserInfo(username) {
     //     });
 }
 
+
+function renderUserInfoTemplate(user) {
+    let userInfo = document.getElementById("userInfo");
+
+    userInfo.setAttribute("class", "row user-info");
+    userInfo.innerHTML = `
+        <div class="col-lg-4">
+            <img src="${user.avatar_url}" alt="avatar">
+            <h4>${user.login}</h4>
+        </div>
+
+        <div class="col-lg-8">
+            ${user.name? `
+                <h2>${user.name}</h2>
+                <p>
+                    <a href="${user.html_url}?tab=followers">
+                        Followers: ${user.followers}
+                    </a>
+                    /
+                    <a href="${user.html_url}?tab=following">
+                        Following: ${user.following}
+                    </a>
+                </p>
+
+                ${user.location? `<p class="location">Location: ${user.location}</p>` : ''}
+                ` : ''}
+            <p>
+                <a class="btn btn-default btn-outline-secondary" href="${user.html_url}" role="button">View details</a>
+            </p>
+        </div>
+    `;
+}
+
+/*
+ User repo listing 
+*/
 function fetchUserRepos() {
     console.log('useRepos', reposJson);
 
@@ -30,3 +70,5 @@ function fetchUserRepos() {
     //         console.log('useRepos', response);
     //     });
 }
+
+
